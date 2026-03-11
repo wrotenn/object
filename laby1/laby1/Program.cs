b0a5627f9f9ce1a2889298b886ab3909097b27db
wrotenn <mineyoupe@gmail.com>
2026-03-11 11:52:43 +0100

    Add files via upload

---
+++ laby1/laby1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+public class FirstProgram
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.Write("hello world!123");
+        Console.WriteLine(" xd");
+        Console.WriteLine(2 + 2); //dodawanie
+        Console.WriteLine(3 - 2); //odejmowanie
+        Console.WriteLine(3 * 3); //mnożenie
+        Console.WriteLine(12 / 4); //dzielenie
+        Console.WriteLine(19 % 4); //reszta z dzielenia
+        Console.WriteLine(2+2*3);
+        Console.WriteLine(100-5*(2+3)-52);
+        /* xdsfsfds
+         sfdsfsfdsfsfsfsf
+         fdsfdsfsfdsfsfs */
+        Console.Write("Cześć!" + "! " + "Lubisz programować w C#?");
+
+        var myVar = 1;
+        Console.WriteLine(myVar);
+        var my2NdVar = "hi";
+        Console.WriteLine(my2NdVar);
+        
+        int number = 1;
+        String newString = "xd";
+        Console.WriteLine(newString);
+        char newChar = 'z';
+        
+        int a = 10, b = 5;
+        var c = a + b;
+        Console.WriteLine("c = " + c);
+        
+        int pi = (int) 3.14f;
+        float e = 2.7f;
+        int eInt = (int) e;
+        Console.WriteLine(pi);
+        Console.WriteLine(eInt);
+        
+        float flok = float pi;
+    }
+}
