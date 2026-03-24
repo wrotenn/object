525baba65ff8d52efef16164a2c66105ea0c13d7
wrotenn <mineyoupe@gmail.com>
2026-03-24 19:16:23 +0100

    d

---
+++ Program23.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Ile liczb calkowitych chcesz podac?:");
+		int ilosc = Convert.ToInt32(Console.ReadLine());
+		int[] tabLiczb = new int[ilosc];
+		for (int i = 0; i < tabLiczb.Length; i++)
+		{
+			Console.WriteLine("Podaj liczbe");
+			tabLiczb[i] = Convert.ToInt32(Console.ReadLine());
+		}
+		
+		Console.WriteLine("Wypisuję liczby parzyste: ");
+		
+		foreach (var parzystosc in tabLiczb)
+		{
+			if(parzystosc % 2 == 0)
+			{
+				Console.Write(parzystosc + ", ");
+			}
+		}
+	}
+}
---
+++ Program4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        dictionary.Add("Policja", "997");
+        dictionary.Add("Straż Pożarna", "998");
+        dictionary.Add("Pogotowie ratunkowe", "999");
+        dictionary.Add("Numer Alarmowy", "112");
+        dictionary.Add("Pogotowie gazowe", "992");
+        dictionary.Add("Choroszcz", "(85) 719 10 91");
+
+        foreach (KeyValuePair<string, string> pair in dictionary)
+        {
+            Console.WriteLine("{0}:{1}", pair.Key,pair.Value);
+        }
+    }
+}
