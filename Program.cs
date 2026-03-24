eb64095ee868d87876de1a28a5db761f3703e064
wrotenn <mineyoupe@gmail.com>
2026-03-24 19:14:45 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+public class program
+{
+    public static void Main(string[] args)
+    {
+        List<string> imiona = new List<string>();
+        imiona.Add("Arkadiusz");
+        imiona.Add("Wiesław");
+        imiona.Add("Stanisław");
+        imiona.Add("Henryk");
+        imiona.Add("Robert");
+        imiona.Add("Władysław");
+        imiona.Add("Ferdynand");
+        imiona.Add("Marian");
+        imiona.Add("Arnold");
+        imiona.Add("Edward");
+
+        for (int i = imiona.Count - 1; i >= 0; i--)
+        {
+            Console.WriteLine(imiona[i]);
+        }
+}
+}
